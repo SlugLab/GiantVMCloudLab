55565125181b875292da4a0233aa0a8d114292c0
arquinn <arquinn@node-0.arquinn-113753.dvm-pg0.wisc.cloudlab.us>
2021-12-20 13:01:31 -0600

    first setup scripts

---
+++ scripts/setup_localdisk.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+
+## My understanding is that you cannot create disk images that use these things.
+## so, like, don't.
+
+sudo mkdir /data
+sudo chmod 777 /data
+sudo /usr/local/etc/emulab/mkextrafs.pl /data
---
+++ scripts/setup_privkey.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+##maybe make ~/.ssh
+mkdir -p $HOME/.ssh && chmod 700 $HOME/.ssh
+
+
+## get the generated RSA private key for the experiment
+geni-get key > $HOME/.ssh/id_rsa
+chmod 600 $HOME/.ssh/id_rsa
+
+## Calculate the public key portion:
+ssh-keygen -y -f $HOME/.ssh/id_rsa > $HOME/.ssh/id_rsa.pub
+
+## make it so the private key gets you login access:
+grep -q -f $HOME/.ssh/id_rsa.pub $HOME/.ssh/authorized_keys || $HOME/.ssh/id_rsa.pub >> $HOME/.ssh/authorized_keys
