61d663443ec00707c1515dc4cb0e43dc9a78cfff
arquinn <arquinn@localhost>
2022-01-28 11:53:49 -0600

    updated giantvm scripts

--- scripts/start_giantvm.sh
+++ scripts/start_giantvm.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-qemu-system-x86_64\
-    -nographic\
-    -kernel /boot/vmlinuz-5.4.0+\
-    -initrd /boot/initrd.img-5.4.0+\
-    -drive file="/data/local/ubuntu.img,format=qcow2"\
-    -drive "file=/data/local/user_data.img,format=raw"\
-    -append "root=/dev/sda1 console=ttyS0"\
-    -cpu host\
-    -machine kernel-irqchip=off\
-    -smp 1\
-    --enable-kvm\
-    -net user\
-    -net nic,model=virtio\
-    -m 4096
+sudo qemu-system-x86_64\
+     -nographic\
+     -drive file="/data/local/ubuntu.img,format=qcow2"\
+     -drive "file=/data/local/user_data.img,format=raw"\
+     -cpu host\
+     -machine kernel-irqchip=off\
+     -smp 1\
+     --enable-kvm\
+     -net user\
+     -net nic,model=virtio\
+     -m 4096
+
+#-kernel /boot/vmlinuz-5.4.0+
+#    -initrd /boot/initrd.img-5.4.0+
+#    -append "root=/dev/sda1 console=ttyS0"
