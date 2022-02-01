441f28be64a69ba18cb0781fead7e48ab9237824
arquinn <arquinn@localhost>
2022-01-31 23:35:25 -0600

    updates to start giantvm

--- scripts/start_giantvm.sh
+++ scripts/start_giantvm.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-sudo qemu-system-x86_64\
+
+sudo /local/src/QEMU/build/x86_64-softmmu/qemu-system-x86_64\
      -nographic\
      -drive file="/data/local/ubuntu.img,format=qcow2"\
      -drive "file=/data/local/user_data.img,format=raw"\
@@ -8,10 +9,12 @@ sudo qemu-system-x86_64\
      -machine kernel-irqchip=off\
      -smp 1\
      --enable-kvm\
-     -net user\
+     -net bridge,br=virbr0,helper=/local/src/QEMU/build/qemu-bridge-helper\
      -net nic,model=virtio\
      -m 4096
 
 #-kernel /boot/vmlinuz-5.4.0+
 #    -initrd /boot/initrd.img-5.4.0+
 #    -append "root=/dev/sda1 console=ttyS0"
+
+#sudo qemu-system-x86_64\
