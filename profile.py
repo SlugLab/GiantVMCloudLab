07287104c53bab92eafbf90cd306090b943e89b1
PoonehSN <PoonehSN>
2022-02-23 08:16:40 -0800

    two nodes

--- profile.py
+++ profile.py
@@ -24,12 +24,21 @@ request = pc.makeRequestRSpec()
 
 # Node node-0
 node_0 = request.RawPC('node-0')
+node_1 = request.RawPC('node-1')
 #node_0.hardware_type = 'c4130'  It's important that we're in the wisc datacenter, but otherwise we can use any available host I think?
 node_0.disk_image = 'urn:publicid:IDN+wisc.cloudlab.us+image+DVM-PG0:GiantVM-Single-Node'
+node_1.disk_image = 'urn:publicid:IDN+wisc.cloudlab.us+image+DVM-PG0:GiantVM-Single-Node'
 
 node_0.addService(pg.Execute(shell="sh", command="/local/repository/scripts/setup_privkey.sh"))
 node_0.addService(pg.Execute(shell="sh", command="/local/repository/scripts/setup_localdisk.sh"))
 node_0.addService(pg.Execute(shell="sh", command="/local/repository/scripts/setup_giantvm.sh"))
+
+node_1.addService(pg.Execute(shell="sh", command="/local/repository/scripts/setup_privkey.sh"))
+node_1.addService(pg.Execute(shell="sh", command="/local/repository/scripts/setup_localdisk.sh"))
+node_1.addService(pg.Execute(shell="sh", command="/local/repository/scripts/setup_giantvm.sh"))
+
+# Create a link between them
+link1 = request.Link(members = [node_0,node_1])
 
 # Print the generated rspec
 pc.printRequestRSpec(request)
